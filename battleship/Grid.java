b3dee33dad9088048d0e2fe164fa5c338c784280
TheFlyingPolak <jakubgajewski14@gmail.com>
2017-11-07 15:49:14 +0000

    Add files via upload
    Changed the way input is received. An Input class reads input in format letter-number (e.g. B5) and translates it into an x and y index.
    Class grid has been divided into 2 subclasses - Primary and Tracking.

--- battleship/Grid.java
+++ battleship/Grid.java
@@ -1,12 +1,12 @@
 /*
-    Class Grid contains a 10x10 array to store ships and methods to place ships on the grid and to
-    facilitate attacks on that grid.
+    Class Grid contains a 10x10 array to store positions of ships. This class is the
+	superclass of Primary and Tracking.
 */
 
 package battleship;
 
 public class Grid {
-    private char[][] grid;  // 10x10 array where ships, hits and misses are stored
+    protected char[][] grid;  // 10x10 array where ships, hits and misses are stored
     
     /*  Constructor for class grid  */
     public Grid(){
@@ -20,116 +20,6 @@ public class Grid {
     }
     
     public char getField(int x, int y){
-	return grid[x][y];
+		return grid[x][y];
     }
-    /*  Method to check the grid to see if an attack is valid, if it was a hit or a miss  */
-    public int checkAttack(int x, int y){
-        if (grid[x][y] == 'H'){
-            grid[x][y] = 'X';
-            return 1;
-        }
-	else if (grid[x][y] == 'o' || grid[x][y] == 'X'){
-	    return -1;
-	}
-        else{
-	    grid[x][y] = 'o';
-            return 0;
-        }
-    }
-    /*  Method to place a ship on the grid during the set-up stage of the game.
-        @param size: the size of the ship. size >= 2, size <= 5
-        @param x, y: the x and y position of the rear end of the ship on the grid. x, y >= 0, x, y <= 9
-        @param dir: direction of the ship
-        @return true if ship placed successfully, false if cannot be placed
-    */
-    public boolean placeShip(int size, int x, int y, ShipDirection dir){
-        switch(dir){
-            case NORTH:
-                if (y - (size - 1) < 0){    // 
-                    return false;
-                }
-                else{
-                    for (int Y = y; Y > y - size; Y--){
-                        if (grid[x][Y] != ' '){
-                            return false;
-                        }
-                    }
-                }
-                for (int Y = y; Y > y - size; Y--){
-                    grid[x][Y] = 'H';
-                }
-                return true;
-            case SOUTH:
-                if (y + (size - 1) > 9){
-                    return false;
-                }
-                else{
-                    for (int Y = y; Y < y + size; Y++){
-                        if (grid[x][Y] != ' '){
-                            return false;
-                        }
-                    }
-                }
-                for (int Y = y; Y < y + size; Y++){
-                    grid[x][Y] = 'H';
-                }
-                return true;
-            case EAST:
-                if (x + (size - 1) > 9){
-                    return false;
-                }
-                else{
-                    for (int X = x; X < x + size; X++){
-                        if (grid[X][y] != ' '){
-                            return false;
-                        }
-                    }
-                }
-                for (int X = x; X < x + size; X++){
-                    grid[X][y] = 'H';
-                }
-                return true;
-            case WEST:
-                if (x - (size - 1) < 0){
-                    return false;
-                }
-                else{
-                    for (int X = x; X > x - size; X--){
-                        if (grid[X][y] != ' '){
-                            return false;
-                        }
-                    }
-                }
-                for (int X = x; X > x - size; X--){
-                    grid[X][y] = 'H';
-                }
-                return true;
-        }
-        return false;
-                
-    }
-    
-    public int checkGrid(int x, int y){ 
-        if (x < 0 || x > 9 || y < 0 || y > 9){
-            System.out.println("Coordinate is outside the grid. Choose a different one!");
-            return -1;
-        }
-        else if (grid[x][y] == 'X' || grid[x][y] == 'o'){
-            System.out.println("You have already taken a shot at this coordinate. Choose a different one!");
-            return -1;
-        }
-        else if (grid[x][y] == 'H'){
-            System.out.println("You have scored a hit!");
-            return 1;
-        }
-        else{
-            System.out.println("You have missed!");
-            return 0;
-        }
-    }
-    
-    public void draw(int x, int y, char c){
-	this.grid[x][y] = c;
-    }
-    
 }
---
+++ battleship/Input.java
@@ -0,0 +1,80 @@
+package battleship;
+
+import java.util.Scanner;
+
+public class Input{
+    private int x;
+    private int y;
+    
+    public Input(){
+	
+    }
+    
+    public void enterCoordinates(){
+	String coordinate;
+	String xAsString;
+	char yAsChar;
+	
+	Scanner scanner = new Scanner(System.in);
+	while(true){
+		System.out.print("Enter coordinates: ");
+		coordinate = scanner.next();
+		yAsChar = coordinate.charAt(0);
+		if (yAsChar >= 'A' && yAsChar <= 'J')
+		    y = yAsChar - 'A';
+		else if (yAsChar >= 'a' && yAsChar <= 'j')
+		    y = yAsChar - 'a';
+		else{
+		    System.out.println("Error: Invalid input. Try again");
+		    continue;
+		}
+		
+		xAsString = coordinate.substring(1, coordinate.length());
+		try{
+		    x = Integer.parseInt(xAsString) - 1;
+		    if (x < 0 || x > 9){
+			System.out.println("Error: Invalid input. Try again");
+			continue;
+		    }
+		}
+		catch (Exception e){
+		    System.out.println("Error: Invalid input. Try again");
+		    continue;
+		}
+		break;
+	    }
+    }
+    
+    public ShipDirection enterDirection(){
+	String string;
+	Scanner scanner = new Scanner(System.in);
+	
+	do{
+            System.out.print("Enter direction: ");
+            string = scanner.next();
+            if (!string.equals("north") && !string.equals("south") && !string.equals("east") && !string.equals("west") && !string.equals("n") && !string.equals("s") && !string.equals("e") && !string.equals("w"))
+                System.out.println("Invalid input. Please enter (n)orth, (s)outh, (e)ast or (w)est");
+            else{
+                switch(string){
+		    case "n":
+                    case "north":   return ShipDirection.NORTH;
+		    case "s":
+                    case "south":   return ShipDirection.SOUTH;
+		    case "e":
+                    case "east":    return ShipDirection.EAST;
+		    case "w":
+                    case "west":    return ShipDirection.WEST;
+                }
+            }
+        }while (!string.equals("east") && !string.equals("west") && !string.equals("north") && !string.equals("south") && !string.equals("n") && !string.equals("s") && !string.equals("e") && !string.equals("w"));
+	return ShipDirection.NORTH;
+    }
+    
+    public int getX(){
+	return x;
+    }
+    
+    public int getY(){
+	return y;
+    }
+}
